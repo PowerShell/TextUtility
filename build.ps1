5f8cf4752419dd65fd299c987812e79bf6ca786e
James Truher [MSFT] <jimtru@microsoft.com>
2023-06-20 20:23:21 -0700

    Changes for release build (#32)
    * changes for build and sign.
    
    * Additional changes for release build.
    
    Update codebase upload.
    update certificate.
    
    * Fix typo in displayname location.
    
    * Fix another typo in displayName.
    
    * Add test parameter for CI.
    
    Mark compare tests as pending.
    
    * Change set-location to the proper location.
    
    * Be sure to publish the correct directory.
    
    Add formatting to files which need signing.
    
    * Fix typo in publish path.
    
    * Reuse already created variables.
    
    Simply the signing pattern.
    
    * Additional changes for signed release build.
    
    * Fix incorrect yaml indentation.
    
    * More incorrect indentation.
    
    * Fix line spacing
    
    * remove displayName for sbom
    
    * Be sure to be in the right directory when creating variables.
    
    * Fix incorrect dictionary.txt location
    
    * Fix typo in nupkg creation step.
    add a target path for the nupkg publishing.
    
    * Change how the nupkg is published
    
    * Copy rather than move the nupkg.
    Also add -verbose for copy

--- build.ps1
+++ build.ps1
@@ -1,22 +1,103 @@
 ## Copyright (c) Microsoft Corporation.
 ## Licensed under the MIT License.
 
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName="build")]
 param (
-    [Parameter()]
+    [Parameter(ParameterSetName="build")]
     [string]
     $Configuration = "Debug",
 
+    [Parameter(ParameterSetName="package")]
+    [switch]
+    $NoBuild,
+
+    [Parameter(ParameterSetName="package")]
+    [switch]
+    $Package,
+
+    [Parameter(ParameterSetName="package")]
+    [switch]
+    $signed,
+
+    [Parameter(ParameterSetName="test")]
+    [switch]
+    $test,
+
     [Parameter()]
     [switch]
-    $Clean
+    $Clean,
+
+    [Parameter()]
+    [switch]
+    $GetPackageVersion
+
 )
+
+
+$moduleFileManifest = @(
+    @{ Sign = $true ; File = "Microsoft.PowerShell.TextUtility.format.ps1xml" }
+    @{ Sign = $true ; File = "Microsoft.PowerShell.TextUtility.psd1" }
+    @{ Sign = $false; File = "dictionary.txt" }
+    @{ Sign = $true ; File = "Microsoft.PowerShell.TextUtility.dll" }
+)
+
+$moduleName = "Microsoft.PowerShell.TextUtility"
+$repoRoot = git rev-parse --show-toplevel
+
+#
+function Get-ModuleInfo {
+    import-powershelldatafile "$repoRoot/src/${moduleName}.psd1"
+}
+
+# this takes the files for the module and publishes them to a created, local repository
+# so the nupkg can be used to publish to the PSGallery
+function Export-Module
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
+    param($packageRoot)
+
+    if ( -not (test-path $packageRoot)) {
+        throw "'$packageRoot' does not exist"
+    }
+    # now construct a nupkg by registering a local repository and calling publish module
+    $repoName = [guid]::newGuid().ToString("N")
+    try {
+        Register-PSRepository -Name $repoName -SourceLocation ${repoRoot} -InstallationPolicy Trusted
+        Publish-Module -Path $packageRoot -Repository $repoName
+    }
+    catch {
+        throw $_
+    }
+    finally {
+        if (Get-PackageSource -Name $repoName) {
+            Unregister-PSRepository -Name $repoName
+        }
+    }
+    Get-ChildItem -Recurse -Name $packageRoot | Write-Verbose -Verbose
+
+    # construct the package path and publish it
+    $nupkgName = "{0}.{1}" -f $moduleName,$moduleInfo.ModuleVersion
+    $pre = $moduleInfo.PrivateData.PSData.Prerelease
+    if ($pre) { $nupkgName += "-${pre}" }
+    $nupkgName += ".nupkg"
+    $nupkgPath = Join-Path $repoRoot $nupkgName
+    if ($env:TF_BUILD) {
+        # In Azure DevOps
+        Write-Host "##vso[artifact.upload containerfolder=$nupkgName;artifactname=$nupkgName;]$nupkgPath"
+    }
+    else {
+        Write-Verbose -Verbose "package path: ${nupkgPath} (exists:$(Test-Path $nupkgPath))"
+    }
+}
 
 try {
     Push-Location "$PSScriptRoot/src/code"
+    $script:moduleInfo = Get-ModuleInfo
+    if ($GetPackageVersion) {
+        return $moduleInfo.ModuleVersion
+    }
 
-    $outPath = "$PSScriptRoot/out/Microsoft.PowerShell.TextUtility"
-
+    $outPath = "$PSScriptRoot/out/${moduleName}"
     if ($Clean) {
         if (Test-Path $outPath) {
             Write-Verbose "Deleting $outPath"
@@ -26,7 +107,37 @@ try {
         dotnet clean
     }
 
-    dotnet publish --output $outPath --configuration $Configuration
+    if (-not $NoBuild) {
+        dotnet publish --output $outPath --configuration $Configuration
+    }
+
+    if ($Test) {
+        $script = [ScriptBlock]::Create("try {
+                Import-Module '${repoRoot}/out/${moduleName}/'
+                Import-Module -Name Pester -Max 4.99
+                Push-Location '${repoRoot}/test'
+                Invoke-Pester
+            }
+            finally {
+                Pop-Location
+            }")
+        pwsh -c $script
+    }
+
+    if ($Package) {
+        if ($Signed) {
+            $pkgBase = "${PSScriptRoot}/signed/${moduleName}"
+        }
+        else {
+            $pkgBase = "${PSScriptRoot}/out/${moduleName}"
+        }
+
+        if (-not (Test-Path $pkgBase)) {
+            throw "Directory '$pkgBase' does not exist"
+        }
+
+        Export-Module -packageRoot $pkgBase
+    }
 }
 finally {
     Pop-Location
