c63637420571ca26a5ac893ee9bdff4b3253b140
James Truher <jimtru@microsoft.com>
2023-06-20 20:56:32 -0700

    Initial commit of text table converter with tests.

--- test/CompareText.tests.ps1
+++ test/CompareText.tests.ps1
@@ -1,57 +1,76 @@
 ## Copyright (c) Microsoft Corporation.
 ## Licensed under the MIT License.
 
-Describe 'Compare-Test tests' {
+Describe 'Compare-Text tests' {
     BeforeAll {
-        $leftText = @"
-This is some
-example text.
-"@
-        $rightText = @"
-  This is other
-example text used!
-"@
-
-        $expectedInline = @"
-
-[0;1;32m  [0mThis is [1;9;31msome[0m[0;1;32mother[0m
-example text[1;9;31m.[0m[0;1;32m used![0m
-
-
-"@
-
-        $expectedSideBySide = @"
-
-[0m1 | [0mThis is [1;9;31msome[0m[0m [0m | [0;1;32m  [0mThis is [0;1;32mother…
-[0m2 | [0mexample text[1;9;31m.[0m[0m | [0mexample text[0;1;32m…
-[0m
-
-
-"@
-
+        $currentOutputRendering = $PSStyle.OutputRendering
+        $PSStyle.OutputRendering = 'Ansi'
+        $leftText = @("This is some", "example text.") -join [Environment]::NewLine
+        $rightText = @("  This is other", "example text used!") -join [Environment]::NewLine
+        $expectedInline = @(
+            ""
+            "``e[0;1;32m  ``e[0mThis is ``e[1;9;31msome``e[0m``e[0;1;32mother``e[0m"
+            "example text``e[1;9;31m.``e[0m``e[0;1;32m used!``e[0m"
+            "" 
+        )
+        $expectedSideBySide = @(
+            ""
+            "``e[0m1 | ``e[0mThis is ``e[1;9;31msome``e[0m``e[0m ``e[0m | ``e[0;1;32m  ``e[0mThis is ``e[0;1;32mother``e…"
+            "``e[0m2 | ``e[0mexample text``e[1;9;31m.``e[0m``e[0m | ``e[0mexample text``e[0;1;32m…"
+            "``e[0m"
+            ""
+        )
+        # this reset text was added in 7.3.0, we need to remove it from the output so the tests can pass on different ps versions.
+        $inlineResetText = "``e[0;1;32m``e[0m``e[1;9;31m``e[0m``e[0;1;32m``e[0m"
+        $sideBySideResetText = "``e[0m``e[0m``e[1;9;31m``e[0m``e[0m``e[0m``e[0;1;32m``e[0m``e[0;1;32m"
     }
 
+    AfterAll {
+        $PSStyle.OutputRendering = $currentOutputRendering
+    }
+
+    # These tests convert the emitted escape sequences to their literal representation
+    # to ease debugging. Also, we use Out-String -Stream to get the output as a collection which Pester 4.10.1 can handle.
     It 'Compare with no specified view uses inline' {
-        Set-ItResult -pending -because "comparison tests are not yet running"
-        $out = Compare-Text -LeftText $leftText -RightText $rightText | Out-String
-        $out | Should -BeExactly $expectedInline
+        $out = Compare-Text -LeftText $leftText -RightText $rightText | Out-String -Stream | Foreach-Object {"$_".Replace("`e","``e").Replace($inlineResetText, "")}
+        $out | Should -Be $expectedInline
     }
 
     It 'Compare with no specified view uses inline and positional parameters' {
-        Set-ItResult -pending -because "comparison tests are not yet running"
-        $out = Compare-Text $leftText $rightText | Out-String
-        $out | Should -BeExactly $expectedInline
+        $out = Compare-Text $leftText $rightText | Out-String -Stream | Foreach-Object {"$_".Replace("`e","``e").Replace($inlineResetText, "")}
+        $out | Should -Be $expectedInline
     }
 
     It 'Compare with inline works' {
-        Set-ItResult -pending -because "comparison tests are not yet running"
-        $out = Compare-Text $leftText $rightText -View Inline | Out-String
-        $out | Should -BeExactly $expectedInline
+        $out = Compare-Text $leftText $rightText -View Inline | Out-String -Stream | Foreach-Object {"$_".Replace("`e","``e").Replace($inlineResetText, "")}
+        $out | Should -Be $expectedInline
     }
 
-    It 'Compare with sideybyside works' {
-        Set-ItResult -pending -because "comparison tests are not yet running"
-        $out = Compare-Text $leftText $rightText -View SideBySide | Out-String
-        $out | Should -BeExactly $expectedSideBySide
+    It 'Compare with sidebyside works' {
+        Set-ItResult -Pending -Because "https://github.com/PowerShell/TextUtility/issues/30"
+        $out = Compare-Text $leftText $rightText -View SideBySide | Out-String -Stream | Foreach-Object {"$_".Replace("`e","``e").Replace($sideBySideResetText, "")}
+        $out | Should -Be $expectedSideBySide
+    }
+
+
+    Context "PlainText comparison" {
+        BeforeAll {
+            $currentRendering = $PSStyle.OutputRendering
+            $PSStyle.OutputRendering = 'PlainText'
+        }
+
+        AfterAll {
+            $PSStyle.OutputRendering = $currentRendering
+        }
+
+        It "Default view should have no escape characters" {
+            (Compare-Text $leftText $rightText | Out-String -Stream) | Should -Not -Match "`e"
+        }
+
+        It "SideBySide view should have no escape characters" {
+            Set-ItResult -Pending -Because "https://github.com/PowerShell/TextUtility/issues/30"
+            (Compare-Text $leftText $rightText -View SideBySide | Out-String -Stream) | Should -Not -Match "`e"
+        }
+
     }
 }
